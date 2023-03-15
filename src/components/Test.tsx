53dfcfa460058479966d2cdbe676187ae0ae1534
pravar <pravar.sharma@trootech.com>
2023-03-15 15:19:52 +0530

    updated immer library to update the objects easily

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import produce from "immer";
+import produce from "immer"; // to handle update easily. else we need to create detechable
 
 const Test = () => {
   const [customer, setCustomer] = useState({
@@ -11,8 +11,8 @@ const Test = () => {
   });
   const [tags, setTag] = useState(["happy", "cheerful"]);
   const [bugs, setBug] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
+    { id: 1, title: "Bug-1", fixed: false },
+    { id: 2, title: "Bug-2", fixed: false },
   ]);
   const handleClick = () => {
     // objects
@@ -21,12 +21,24 @@ const Test = () => {
     //   name: "Pravar",
     //   address: { ...customer.address, city: "Alwar", zipCode: 301001 },
     // });
+    setCustomer(
+      produce((draft) => {
+        draft.name = "Pravar";
+        draft.address.city = "Alwar";
+        draft.address.zipCode = 301001;
+        return draft;
+      })
+    );
     // arrays
     // const newTag = [...tags, "newTag"];
     // setTag(newTag.filter((tag) => tag !== "happy"));
-    // setTag(produce(draft=>{
-    //     const tag
-    // }))
+    setTag(
+      produce((draft) => {
+        let tags = draft.filter((tag) => tag !== "happy");
+        tags.push("newTag");
+        return tags;
+      })
+    );
     // array of objects
     // setBug(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
     setBug(
@@ -44,7 +56,12 @@ const Test = () => {
       {customer.address.zipCode}
       <button onClick={handleClick}>Click</button>
       {tags.map((e) => e)}
-      {bugs.map((bug) => (bug.fixed ? bug.id : ""))}
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title}
+          {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
     </div>
   );
 };
